8bd2ffdd9b3fc019a12b94a7765ee5f5b0394c82
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-17 16:22:41 +0800

    add 36

---
+++ 36.cpp
@@ -0,0 +1,49 @@
+// 36. Valid Sudoku
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  bool isValidSudoku(vector<vector<char>> &board) {
+    for (vector<char> row : board) {
+      vector<int> record(10, 1);
+      for (char c : row) {
+        if (c == '.')
+          continue;
+        int num = c - '0';
+        if (--record[num] < 0)
+          return false;
+      }
+    }
+
+    for (int i = 0; i < 9; ++i) {
+      vector<int> record(10, 1);
+      for (vector<char> row : board) {
+        char c = row[i];
+        if (c == '.')
+          continue;
+        int num = c - '0';
+        if (--record[num] < 0)
+          return false;
+      }
+    }
+
+    for (int r = 0; r < 3; ++r) {
+      for (int c = 0; c < 3; ++c) {
+        vector<int> record(10, 1);
+        for (int i = 0; i < 3; ++i) {
+          for (int j = 0; j < 3; ++j) {
+            char ch = board[r * 3 + i][c * 3 + j];
+            if (ch == '.')
+              continue;
+            int num = ch - '0';
+            if (--record[num] < 0)
+              return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+};
