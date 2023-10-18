869b6ad7bc833f99e451af836ecb24f4a5597546
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-18 21:48:19 +0800

    add 424

---
+++ 424.cpp
@@ -0,0 +1,33 @@
+// 424. Longest Repeating Character Replacement
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+  int characterReplacement(string s, int k) {
+    int length = s.length();
+    unordered_map<char, int> m;
+    int l = 0, r = 0;
+    int windowSize = 0;
+    for (; r < length; ++r) {
+      m[s[r]]++;
+      auto it =
+          max_element(m.begin(), m.end(),
+                      [](const pair<char, int> &a, const pair<char, int> &b) {
+                        return a.second < b.second;
+                      });
+      while (it->second + k < r - l + 1) {
+        m[s[l++]]--;
+        it =
+            max_element(m.begin(), m.end(),
+                        [](const pair<char, int> &a, const pair<char, int> &b) {
+                          return a.second < b.second;
+                        });
+      }
+      windowSize = windowSize < r - l + 1 ? r - l + 1 : windowSize;
+    }
+
+    return windowSize;
+  }
+};
