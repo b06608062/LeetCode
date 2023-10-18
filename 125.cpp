6849ea7eda7d7bb71330bb74d93a72d5230f461d
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-18 11:48:49 +0800

    add 125

---
+++ 125.cpp
@@ -0,0 +1,22 @@
+// 125. Valid Palindrome
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  bool isPalindrome(string s) {
+    string t = "";
+    for (char c : s) {
+      if (isalpha(c) || isdigit(c))
+        t += towlower(c);
+    }
+    int length = t.length();
+    int half = length / 2;
+    for (int i = 0; i < half; i++) {
+      if (t[i] != t[length - i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
