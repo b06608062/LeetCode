dd1482edaada298197fba7683bd901141cd57fe6
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-20 00:59:59 +0800

    add 143

---
+++ 143.cpp
@@ -0,0 +1,36 @@
+// 143. Reorder List
+#include <stack>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  void reorderList(ListNode *head) {
+    stack<ListNode *> st;
+    ListNode *p = head;
+    int count = 0;
+    while (p != nullptr) {
+      st.push(p);
+      count++;
+      p = p->next;
+    }
+
+    ListNode *next;
+    p = head;
+    for (int i = 1; i <= count / 2; ++i) {
+      next = p->next;
+      p->next = st.top();
+      st.pop();
+      p->next->next = next;
+      p = next;
+    }
+    p->next = nullptr;
+  }
+};
