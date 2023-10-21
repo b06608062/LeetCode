992f715976ff4298ac593fecf68db96476e3556c
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-21 18:07:17 +0800

    add 572

---
+++ 572.cpp
@@ -0,0 +1,42 @@
+// 572. Subtree of Another Tree
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  bool isSubtree(TreeNode *root, TreeNode *subRoot) {
+    if (root != nullptr) {
+      return isSameTree(root, subRoot) || isSubtree(root->left, subRoot) ||
+             isSubtree(root->right, subRoot);
+    }
+
+    return false;
+  }
+
+  bool isSameTree(TreeNode *p, TreeNode *q) {
+    if (p == nullptr && q == nullptr)
+      return true;
+
+    bool res = true;
+    if (p != nullptr && q != nullptr) {
+      if (p->val != q->val) {
+        res = false;
+      } else {
+        res = isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+      }
+    } else {
+      res = false;
+    }
+
+    return res;
+  }
+};
