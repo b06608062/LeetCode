a444954abcb56a66a95fee2b240870d30f3a9d84
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-21 14:19:42 +0800

    add 287

---
+++ 287.cpp
@@ -0,0 +1,18 @@
+// 287. Find the Duplicate Number
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int findDuplicate(vector<int> &nums) {
+    sort(nums.begin(), nums.end(), [](int &a, int &b) { return a < b; });
+    int length = nums.size();
+    for (int i = 1; i < length; ++i) {
+      if (nums[i] == nums[i - 1])
+        return nums[i];
+    }
+
+    return -1;
+  }
+};
