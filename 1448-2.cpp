4c23ed816130160ffcf6742d41030b2c51127600
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-21 21:47:47 +0800

    add 1448 1448-2

---
+++ 1448-2.cpp
@@ -0,0 +1,39 @@
+// 1448. Count Good Nodes in Binary Tree
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int goodNodes(TreeNode *root) {
+    if (root == nullptr)
+      return 0;
+
+    int count = 0;
+    stack<pair<TreeNode *, int>> s;
+    s.push({root, root->val});
+    while (!s.empty()) {
+      auto [node, max] = s.top();
+      s.pop();
+      if (node->val >= max) {
+        count++;
+        max = node->val;
+      }
+      if (node->left != nullptr)
+        s.push({node->left, max});
+      if (node->right != nullptr)
+        s.push({node->right, max});
+    }
+
+    return count;
+  }
+};
---
+++ 1448.cpp
@@ -0,0 +1,33 @@
+// 1448. Count Good Nodes in Binary Tree
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int count = 0;
+
+  int goodNodes(TreeNode *root) {
+    countGoodNodes(root, -10001);
+    return count;
+  }
+
+  void countGoodNodes(TreeNode *root, int max) {
+    if (root->val >= max) {
+      count++;
+      max = root->val;
+    }
+    if (root->left != nullptr)
+      countGoodNodes(root->left, max);
+    if (root->right != nullptr)
+      countGoodNodes(root->right, max);
+  }
+};
