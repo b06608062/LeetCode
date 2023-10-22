9e0c78072e8426249b16c0e274d6d3d75180f3e2
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-22 10:28:36 +0800

    add 215

---
+++ 215.cpp
@@ -0,0 +1,17 @@
+// 215. Kth Largest Element in an Array
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  priority_queue<int, vector<int>, greater<int>> pq;
+  int findKthLargest(vector<int> &nums, int k) {
+    pq = priority_queue<int, vector<int>, greater<int>>(nums.begin(),
+                                                        nums.end());
+    while (pq.size() > k)
+      pq.pop();
+
+    return pq.top();
+  }
+};
