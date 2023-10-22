44a96a8d910a6cc05bd42bf658c6f2445b5bbeb7
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-23 00:05:13 +0800

    add 39

---
+++ 39.cpp
@@ -0,0 +1,33 @@
+// 39. Combination Sum
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> ans;
+  vector<int> candidates;
+  int target;
+
+  vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
+    this->candidates = candidates;
+    this->target = target;
+    vector<int> subset;
+    build(subset, 0, 0);
+
+    return ans;
+  }
+
+  void build(vector<int> &subset, int i, int sum) {
+    if (sum > target || i == candidates.size()) {
+      return;
+    } else if (sum == target) {
+      ans.push_back(subset);
+    } else {
+      int candidate = candidates[i];
+      subset.push_back(candidate);
+      build(subset, i, sum + candidate);
+      subset.pop_back();
+      build(subset, i + 1, sum);
+    }
+  }
+};
