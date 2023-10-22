c97e74fb9d8d511b9e96aec4bb009e5f1311af65
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-22 23:24:37 +0800

    add 78

---
+++ 78.cpp
@@ -0,0 +1,30 @@
+// 78. Subsets
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> ans;
+  vector<int> nums;
+
+  vector<vector<int>> subsets(vector<int> &nums) {
+    this->nums = nums;
+    vector<int> subset;
+    build(subset, 0);
+
+    return ans;
+  }
+
+  void build(vector<int> &subset, int i) {
+    if (i == nums.size()) {
+      ans.push_back(subset);
+      return;
+    }
+
+    int num = nums[i];
+    build(subset, i + 1);
+    subset.push_back(num);
+    build(subset, i + 1);
+    subset.pop_back();
+  }
+};
