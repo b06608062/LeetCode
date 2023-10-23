b49553315083b16df7becbcb9eeabe3e34d9ecce
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-23 11:53:41 +0800

    add 40

---
+++ 40.cpp
@@ -0,0 +1,38 @@
+// 40. Combination Sum II
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> candidates;
+  int length;
+  vector<vector<int>> ans;
+
+  vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
+    sort(candidates.begin(), candidates.end());
+    this->candidates = candidates;
+    length = candidates.size();
+    vector<int> subset;
+    helper(subset, 0, target);
+
+    return ans;
+  }
+
+  void helper(vector<int> &subset, int start, int target) {
+    if (target < 0) {
+      return;
+    } else if (target == 0) {
+      ans.push_back(subset);
+    } else {
+      for (int i = start; i < length; ++i) {
+        int num = candidates[i];
+        if (i != start && candidates[i] == candidates[i - 1])
+          continue;
+        subset.push_back(num);
+        helper(subset, i + 1, target - num);
+        subset.pop_back();
+      }
+    }
+  }
+};
