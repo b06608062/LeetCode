9f1af8187bb805b283282355188fbe7ecb9d4f54
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-24 10:38:19 +0800

    add 207

---
+++ 207.cpp
@@ -0,0 +1,39 @@
+// 207. Course Schedule
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> graph;
+  vector<int> states;
+  bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+    this->graph = vector<vector<int>>(numCourses);
+    for (auto p : prerequisites) {
+      graph[p[1]].push_back(p[0]);
+    }
+
+    // states: 0 = unkown, 1 = visiting, 2 = visited
+    this->states = vector<int>(numCourses);
+    for (int i = 0; i < numCourses; ++i) {
+      if (dfs(i))
+        return false;
+    }
+
+    return true;
+  }
+
+  bool dfs(int i) {
+    if (states[i] == 2)
+      return false;
+    else if (states[i] == 1)
+      return true;
+    states[i] = 1;
+    for (auto j : graph[i]) {
+      if (dfs(j))
+        return true;
+    }
+    states[i] = 2;
+
+    return false;
+  }
+};
