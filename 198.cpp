c4fe981b69f3c6baf5f89ad8f45cd7aedc9fefba
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2023-10-30 08:13:39 +0800

    ...

---
+++ 198.cpp
@@ -0,0 +1,24 @@
+// 198. House Robber
+#include <math.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int rob(vector<int> &nums) {
+    int size = nums.size();
+    if (size == 0)
+      return 0;
+    else if (size == 1)
+      return nums[0];
+
+    vector<int> moneys(size + 1);
+    moneys[0] = 0;
+    moneys[1] = nums[0];
+    for (int i = 2; i <= size; ++i) {
+      moneys[i] = max(moneys[i - 1], moneys[i - 2] + nums[i - 1]);
+    }
+
+    return moneys[size];
+  }
+};
---
+++ 213.cpp
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int rob(vector<int> &nums) {
+    int l = nums.size();
+    if (l == 0)
+      return 0;
+    else if (l == 1)
+      return nums[0];
+
+    vector<int> in1(l + 1);
+    in1[0] = 0;
+    in1[1] = nums[0];
+    in1[2] = nums[0];
+    vector<int> ex1(l + 1);
+    ex1[0] = 0;
+    ex1[1] = 0;
+    for (int i = 3; i < l; i++)
+      in1[i] = max(in1[i - 1], in1[i - 2] + nums[i - 1]);
+
+    for (int i = 2; i <= l; i++)
+      ex1[i] = max(ex1[i - 1], ex1[i - 2] + nums[i - 1]);
+
+    return max(in1[l - 1], ex1[l]);
+  }
+};
